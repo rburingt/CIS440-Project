4b62ac6605650e0766b45a68a81631ae6d0ea1d8
Ben Smith <bensmith@Mercury.local>
2024-07-27 13:10:43 -0600

    Add login code to check against users.json

---
+++ ProjectTemplate/login.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('#login-button').click(function (event) {
+        event.preventDefault();
+
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        $.getJSON('users.json', function (data) {
+            var userFound = false;
+            var isAdmin = false;
+
+            $.each(data, function (key, user) {
+                if (user.username === username && user.password === password) {
+                    userFound = true;
+                    isAdmin = user.isAdmin;
+                    return false; // Break out of the $.each loop
+                }
+            });
+
+            if (userFound) {
+                if (isAdmin) {
+
+                    // Link to the admin page goes here
+                    window.location.href = 'adminview.html';
+                } else {
+
+                    // Link to the user home page goes here
+                    window.location.href = 'userHome.html';
+                }
+            } else {
+                alert('Invalid username or password.');
+            }
+        });
+    });
+});
