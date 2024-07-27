260ccda2258d08cd6c43eb78cb1cd23bbb0d07d8
juwan-sebastian <jsebast9@asu.edu>
2024-07-27 11:14:42 -0400

    -m "Stores user input into a function. Added functionality to check if form is valid"

--- ProjectTemplate/grievance.js
+++ ProjectTemplate/grievance.js
@@ -21,8 +21,55 @@ function populateDropdown() {
         
         // Append the option to the dropdown
         dropdown.appendChild(option);
-    });s
+    });
 }
+
+//generates complaint number
+function generateComplaintNumber(max) {
+    
+    return Math.floor(Math.random()*max)
+}
+
+formValid = false;
+//stores user input and gives them a complaint number
+function submitForm() {
+   let leaderSelected = document.getElementById("leaderSelected").value;
+   let grievanceType = document.getElementById("grievanceDropdown").value;
+   let offenseText = document.getElementById("offenseTextDescription").value;
+   let complaintNumber = generateComplaintNumber(99999);
+
+   let validity = true;
+
+   var form = document.getElementById('userComplaintForm');
+    for(var i=0; i < form.elements.length; i++){
+      if(form.elements[i].value === '' && form.elements[i].hasAttribute('required')){
+        document.getElementById("formSubmitted").innerHTML = "Please input the missing values";
+        return false;
+      }
+    else {
+        document.getElementById("formSubmitted").innerHTML = "Thank you for submitting your grievance your complaint ID is: " + complaintNumber;
+        return true;
+      }
+    }
+    
+    formSubmission(submitForm());
+    }
+    
+    //attempts to append to formSubmitted ID after submitting form and giving the customer a complaintID
+   
+  
+
+    function formSubmission(validity){
+        let complaintNumber = generateComplaintNumber(99999);
+       
+        if (validity) {
+        document.getElementById("formSubmitted").innerHTML = "Thank you for submitting your grievance your complaint ID is: " + complaintNumber;
+        }
+    }
+  
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', populateDropdown);
